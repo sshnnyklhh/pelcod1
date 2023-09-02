241ed1a46d759b34c1e924cfd437c769ca56dcd0
sshnnyklhh <143860092+sshnnyklhh@users.noreply.github.com>
2023-09-02 13:34:08 +0700

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,19 @@
+nama_saya = "nama saya sheny"
+print(nama_saya,"bertipe", type(nama_saya))
+
+umur = 18
+print(umur,"bertipe",type (umur))
+
+IPK = 3,89
+print(IPK,"bertipe",type (IPK))
+
+menikah = False
+print ("menikah?"),menikah,"bertipe",type(menikah))
+
+matkul_diambil = ["alpro","sig"]
+print("matkul: ",matkul_diambil, "bertipe",type(matkul_diambil))
+
+matkul_diambil2 = ("web","pemvis")
+print("Matkul_Lain: ",matkul_diambil2, "bertipe", type(matkul_diambil2))
+
+matkul_diambil3={"alpro","ALpro","pemweb","data sciene","data sciene"}
